d2e22bbe36ae93779fd99c81c6137da96e5066e8
thanhtx-bitkoex <thanhtx@bitkoex.com>
2018-07-24 17:59:58 +0700

    ok

--- app/utils/CommonStyles.js
+++ app/utils/CommonStyles.js
@@ -127,10 +127,8 @@ const CommonStyles = {
     flex: 1,
   },
   checkBox: {
-    checkedImage: (<Image style={{ width: scale(13), height: scale(13) }}
-                          source={require('../../assets/common/checkbox/checked.png')}/>),
-    unCheckedImage: (<Image style={{ width: scale(13), height: scale(13) }}
-                            source={require('../../assets/common/checkbox/unchecked.png')}/>)
+    checkedImage: (<Image style={{ width: scale(13), height: scale(13) }} source={require('../../assets/common/checkbox/checked.png')}/>),
+    unCheckedImage: (<Image style={{ width: scale(13), height: scale(13) }} source={require('../../assets/common/checkbox/unchecked.png')}/>)
   },
   switch: {
     activeText: 'ON',
